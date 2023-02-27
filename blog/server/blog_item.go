dcafe7dea6147f149d5bcf74b33e48741a4d3e0c
nikzayn <nikhilvaidyar1997@gmail.com>
2023-02-27 22:38:45 +0530

    Blog item schema --added

---
+++ blog/server/blog_item.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	pb "github.com/nikzayn/blog-grpc/blog/proto"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogItem struct {
+	ID       primitive.ObjectID `bson:"_id, omitempty"`
+	AuthorId string             `bson:"author_id"`
+	Title    string             `bson:"title"`
+	Content  string             `bson:"content"`
+}
+
+func documentToBlog(data *BlogItem) *pb.Blog {
+	return &pb.Blog{
+		Id:       data.ID.Hex(),
+		AuthorId: data.AuthorId,
+		Title:    data.Title,
+		Content:  data.Content,
+	}
+}
