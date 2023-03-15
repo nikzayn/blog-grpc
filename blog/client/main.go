56984695879a81dafcadc738b8789a940808a98d
nikzayn <nikhilvaidyar1997@gmail.com>
2023-03-15 23:39:09 +0530

    main.go added for client

---
+++ blog/client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/nikzayn/blog-grpc/blog/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("Failed to connect: %v\n", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewBlogServiceClient(conn)
+
+	id := createBlog(c)
+	readBlog(c, id)               //valid
+	readBlog(c, "aNonExistingId") //invalid
+	// updateBlog(c, id)
+}
